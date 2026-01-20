4f7a65d89f08943db61144c1f6e03a23be8dce79
geoigeo <geoigeo1630gg@gmail.com>
2026-01-20 11:15:34 -0300

    corrigindo novamente

--- js/script.js
+++ js/script.js
@@ -22,3 +22,15 @@ document.querySelector(".prev")?.addEventListener("click", () => {
   index = (index - 1 + fotos.length) % fotos.length;
   imagem.src = fotos[index];
 });
+
+<script>
+document.addEventListener("DOMContentLoaded", () => {
+  const imgs = document.querySelectorAll("img");
+
+  imgs.forEach(img => {
+    if (!img.hasAttribute("loading")) {
+      img.setAttribute("loading", "lazy");
+    }
+  });
+});
+</script>
